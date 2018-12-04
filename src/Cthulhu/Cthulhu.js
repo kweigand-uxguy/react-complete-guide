3201cf9c59bf6f331070f49ee2ffcea85baaef46
Weigand <Kenweigand.uxguy@gmail.com>
2018-12-04 13:54:58 -0600

    Initial commit

---
+++ src/Cthulhu/Cthulhu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Cthulhu.css";
+
+export default cthulhu;
+
+const cthulhu = props => {
+  return (
+    <div className="Cthulhu">
+      <p>I am Cthulhu</p>
+      <p>Souls are a tasty snack</p>
+    </div>
+  );
+};
